b34dbaa7702a2557661b79da651dd3c17a7cb785
Lauryn <laurynpearson@gmail.com>
2019-08-11 14:35:08 -0500

    add sign up, login and logout

--- src/router.js
+++ src/router.js
@@ -3,6 +3,9 @@ import Router from 'vue-router';
 import Home from './views/Home.vue';
 import routesIndex from './views/routesIndex.vue';
 import routeCreate from './views/routeCreate.vue';
+import Signup from './views/Signup.vue';
+import Login from './views/Login.vue';
+import Logout from './views/Logout.vue';
 
 
 Vue.use(Router);
@@ -25,6 +28,21 @@ export default new Router({
       path: '/routes/new',
       name: 'route-new',
       component: routeCreate
+    },
+    {
+      path: '/signup',
+      name: 'signup',
+      component: Signup
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login
+    },
+    {
+      path: '/logout',
+      name: 'logout',
+      component: Logout
     },
     {
       path: '/about',
