8288b0005fd4bd9b9b3af76512e5f1bda6fc22e1
Lauryn <laurynpearson@gmail.com>
2019-08-12 16:46:15 -0500

    add Route and Shoe links to homepage

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="home">
     <h1>{{ message }}</h1>
+    <h2>Routes</h2>
+      <router-link to="/routes">Routes</router-link>
+    <h2>Shoes</h2>
+    <router-link to="/shoes">Shoes</router-link>
+    <p><iframe width="500" height="300" src="https://api.maptiler.com/maps/hybrid/?key=MtE9MrAXmvD6F55I34m5#" frameborder="0"></iframe></p>
   </div>
 </template>
 
@@ -11,7 +16,7 @@
 export default {
   data: function() {
     return {
-      message: "Welcome to Vue.js!"
+      message: "Welcome to the Running App!"
     };
   },
   created: function() {},
