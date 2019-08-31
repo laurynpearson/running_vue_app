a10ca902e7b345ebae4075f7dd586355fad4b24f
Lauryn <laurynpearson@gmail.com>
2019-08-30 20:07:41 -0500

    create race show file and add autocomplete to search bar

--- src/views/raceIndex.vue
+++ src/views/raceIndex.vue
@@ -1,14 +1,17 @@
 <template>
   <div class="Races">
     <h1>{{ message }}</h1>
-    <p>Search: <input type="text" v-model="searchTerm"></p>
-    <div v-for="race in filterBy(races, searchTerm, 'start_date_time')">
+    <p>Search: <input type="text" v-model="searchTerm" list="names"></p>
+    <datalist id="names">
+      <option v-for="race in races">{{race.name}}</option>
+    </datalist>
+    <div v-for="race in filterBy(races, searchTerm, 'name')">
       <h2>Name: {{ race.name }}</h2>
-      <h2>Date: {{ race.start_date_time }}</h2>
-      <h2>Distance: {{ race.distance }}</h2>
-      <h2>City: {{ race.city }}</h2>
-      <h2>State: {{ race.state }}</h2>
-      <h2>Country: {{ race.country }}</h2>
+      <p>Date: {{ race.start_date_time }}</p>
+      <p>Distance: {{ race.distance }}</p>
+      <p>City: {{ race.city }}</p>
+      <p>State: {{ race.state }}</p>
+      <p>Country: {{ race.country }}</p>
       <hr>
     </div>
   </div>
@@ -19,13 +22,16 @@
 <script>
 import axios from 'axios';
 import Vue2Filters from "vue2-filters";
+import uniq from 'lodash/uniq';
 export default {
   mixins: [Vue2Filters.mixin],
   data: function() {
     return {
       message: "Welcome to the Races!",
       races: [],
-      searchTerm: ""
+      searchTerm: "",
+      sortAttribute: "name",
+      selectedCountry: ""
     };
   },
   created: function() {
@@ -33,6 +39,11 @@ export default {
       this.races = response.data;
     });
   },
-  methods: {}
+  methods: {
+    setSortAttribute: function(attribute) {
+      console.log(attribute);
+      this.setSortAttribute = attribute;
+    }
+  }
 };
 </script>
---
+++ src/views/raceShow.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="home">
+    <h1>{{ message }}</h1>
+    <div>
+      <h2>Name: {{ race.name }}</h2>
+      <h2>Date: {{ race.start_date_time }}</h2>
+      <h2>Distance: {{ race.distance }}</h2>
+      <h2>City: {{ race.city }}</h2>
+      <h2>State: {{ race.state }}</h2>
+      <h2>Country: {{ race.country }}</h2>
+    </div>
+  </div>
+</template>
+
+<style>
+</style>
+
+<script>
+import axios from "axios";
+export default {
+  data: function() {
+    return {
+      message: "Welcome to Race Show!",
+      shoe: {}
+    };
+  },
+  created: function() {
+    console.log('in the create)');
+    axios.get('/api/races/' + this.$route.params.id).then(response => {
+      console.log(response.data);
+      this.race = response.data;
+    });
+  },
+  methods: {}
+};
+</script>
