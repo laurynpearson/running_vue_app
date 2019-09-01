f0efe8a1ea8ab7bbb2d634b4a8e48d6a8335f637
Lauryn <laurynpearson@gmail.com>
2019-09-01 11:19:42 -0500

    add dropdown filter for countries

--- src/views/raceIndex.vue
+++ src/views/raceIndex.vue
@@ -2,10 +2,19 @@
   <div class="Races">
     <h1>{{ message }}</h1>
     <p>Search: <input type="text" v-model="searchTerm" list="names"></p>
+    <select v-model="selectedCountry">
+      <option disabled value="">Please select one</option>
+      <option value="">All</option>
+      <option v-for="race in raceCountries">
+        {{ race.country }}
+      </option>
+    </select>
+    <span>Selected: {{ selectedCountry }}</span>
     <datalist id="names">
       <option v-for="race in races">{{race.name}}</option>
     </datalist>
-    <div v-for="race in filterBy(races, searchTerm, 'name')">
+    <!-- <div v-for="race in filterBy(races, searchTerm, 'name')"> -->
+    <div v-for="race in filterBy(races, selectedCountry, 'country')">
       <h2>Name: {{ race.name }}</h2>
       <p>Date: {{ race.start_date_time }}</p>
       <p>Distance: {{ race.distance }}</p>
@@ -13,6 +22,7 @@
       <p>State: {{ race.state }}</p>
       <p>Country: {{ race.country }}</p>
       <hr>
+    <!-- </div> -->
     </div>
   </div>
 </template>
@@ -22,7 +32,7 @@
 <script>
 import axios from 'axios';
 import Vue2Filters from "vue2-filters";
-import uniq from 'lodash/uniq';
+import uniqBy from 'lodash/uniqBy';
 export default {
   mixins: [Vue2Filters.mixin],
   data: function() {
@@ -38,6 +48,11 @@ export default {
     axios.get('/api/races').then(response => {
       this.races = response.data;
     });
+  },
+  computed: {
+    raceCountries() {
+      return uniqBy(this.races, 'country');
+    }
   },
   methods: {
     setSortAttribute: function(attribute) {
